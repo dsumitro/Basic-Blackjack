b9af770b9172e7cc272175e7251337fd53bdef15
Dean Sumitro <DSumitro@talentpath.com>
2019-07-11 15:43:51 -0700

    MVP with ESlint

--- blackjackfp.js
+++ blackjackfp.js
@@ -1,6 +1,5 @@
 function createGame() {
   let deck = createDeck();
-  console.log(deck);
   let result = createCardHolder(deck);
   // TODO: IMPROVE ON THIS DEAN
   const player = { hand: [], handValue: 0 };
@@ -15,9 +14,8 @@ function createGame() {
   dealer.handValue = result.handValue;
   deck = result.newDeck;
   //-----------------------------------
-  console.log(player);
-  console.log(dealer);
-  console.log(deck);
+  const winningMessage = compareHands(player, dealer);
+  printGame(player, dealer, winningMessage);
 }
 function createDeck() {
   const cards = [];
@@ -68,6 +66,44 @@ function getCardValue(weight) {
   }
   return weight;
 }
-function compareHands() {}
-function printResults() {}
+function compareHands(p1, p2) {
+  // plater 1 player
+  // player 2 is dealer
+  const player1Score = p1.handValue;
+  const player2Score = p2.handValue;
+  if (player2Score > 21) {
+    return '\tDEALER IS OVER 21, DEALER LOSES. YOU WIN!';
+  }
+  if (player1Score > 21) {
+    return '\tYOU ARE OVER 21, YOU LOSE';
+  }
+  if (player1Score > player2Score) {
+    return '\tYOU WON';
+  }
+  if (player1Score === player2Score) {
+    return '\tITS A TIE...';
+  }
+  return '\tDEALER WON, YOU LOSE';
+}
+function printGame(player1, player2, winningMessage) {
+  console.clear();
+  console.log(
+    '.------..------..------..------..------.     .------..------..------..------.\n' +
+      '|B.--. ||L.--. ||A.--. ||C.--. ||K.--. |.-.  |J.--. ||A.--. ||C.--. ||K.--. |\n' +
+      '| :(): || :/ : || (  ) || :/ : || :/ : ((5)) | :(): || (  ) || :/ : || :/ : |\n' +
+      "| ()() || (__) || :  : || :  : || :  : |'-.-.| ()() || :  : || :  : || :  : |\n" +
+      "| '--'B|| '--'L|| '--'A|| '--'C|| '--'K| ((2)) '--'J|| '--'A|| '--'C|| '--'K|\n" +
+      "`------'`------'`------'`------'`------'  '-'`------'`------'`------'`------'"
+  );
+  console.log("Dealer's Hand:");
+  console.log(`\t${player2.hand[0].weight} of ${player2.hand[0].suit}`);
+  console.log(`\t${player2.hand[1].weight} of ${player2.hand[1].suit}`);
+  console.log(`Dealer's Score: ${player2.handValue}`);
+  console.log();
+  console.log("Player's Hand:");
+  console.log(`\t${player1.hand[0].weight} of ${player1.hand[0].suit}`);
+  console.log(`\t${player1.hand[1].weight} of ${player1.hand[1].suit}`);
+  console.log(`Player's Score: ${player1.handValue}`);
+  console.log(winningMessage);
+}
 createGame();
