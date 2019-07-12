482972497788751b3e35696ff913aa40c993e054
Dean Sumitro <DSumitro@talentpath.com>
2019-07-12 08:02:07 -0700

    MVP commented

--- blackjackfp.js
+++ blackjackfp.js
@@ -1,14 +1,22 @@
+/**
+ *  create a new function playGame()
+ * takes the deck made and the two players (output of createGame?)
+ * outputs winningMessage(?)
+ */
+/**
+ * Creates the game and executes the logic
+ */
 function createGame() {
   let deck = createDeck();
   let result = createCardHolder(deck);
-  // TODO: IMPROVE ON THIS DEAN
+  // TODO: IMPROVE ON THIS
   const player = { hand: [], handValue: 0 };
   player.hand = result.hand;
   player.handValue = result.handValue;
   deck = result.newDeck;
   //-----------------------------------
   result = createCardHolder(deck);
-  // TODO: IMPROVE ON THIS DEAN
+  // TODO: IMPROVE ON THIS
   const dealer = { hand: [], handValue: 0 };
   dealer.hand = result.hand;
   dealer.handValue = result.handValue;
@@ -17,6 +25,10 @@ function createGame() {
   const winningMessage = compareHands(player, dealer);
   printGame(player, dealer, winningMessage);
 }
+/**
+ * Creates a deck
+ * @returns {obj[]} An array of card objects
+ */
 function createDeck() {
   const cards = [];
   const weights = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'Jack', 'Queen', 'King', 'Ace'];
@@ -29,9 +41,19 @@ function createDeck() {
   });
   return shuffle(cards);
 }
+/**
+ * Shuffles the array of card objects
+ * @param {obj[]} cards
+ * @returns {obj[]} An array of card objects
+ */
 function shuffle(cards) {
   return cards.sort(() => Math.random() - 0.5);
 }
+/**
+ * Creates a cardHolder for the game such as a player
+ * @param {obj[]} deck An array of card objects
+ * @returns {obj{obj[],obj[],int}} An object containing the new deck, player's hand, and player's hand value
+ */
 function createCardHolder(deck) {
   const result = drawCard(deck);
   const { hand } = result;
@@ -39,6 +61,11 @@ function createCardHolder(deck) {
   const handValue = calculateValue(hand);
   return { newDeck, hand, handValue };
 }
+/**
+ * Draws a card from the deck
+ * @param {obj[]} deck An array of card objects
+ * @returns {obj{hand[], newDeck[]}} Returns and object with a hand array and
+ */
 function drawCard(deck) {
   const hand = deck.slice(0, 2);
   const newDeck = pureSpliceThatWeDidntTakeFromAndrew(deck);
@@ -54,6 +81,11 @@ function calculateValue(hand) {
     0
   );
 }
+/**
+ * Converts weight value such as ACE, KING, QUEEN, and JACK to calculatable integers
+ * @param {int or string} weight
+ * @returns {int} The value of the card
+ */
 function getCardValue(weight) {
   if (typeof weight === 'string') {
     const weights = {
@@ -66,6 +98,12 @@ function getCardValue(weight) {
   }
   return weight;
 }
+/**
+ * Determines who won
+ * @param {obj{hand[],handValue}} p1
+ * @param {obj{hand[],handValue}} p2
+ * @returns {string} Winning / losing message
+ */
 function compareHands(p1, p2) {
   // plater 1 player
   // player 2 is dealer
@@ -73,18 +111,22 @@ function compareHands(p1, p2) {
   const player2Score = p2.handValue;
   if (player2Score > 21) {
     return '\tDEALER IS OVER 21, DEALER LOSES. YOU WIN!';
-  }
-  if (player1Score > 21) {
+  } if (player1Score > 21) {
     return '\tYOU ARE OVER 21, YOU LOSE';
-  }
-  if (player1Score > player2Score) {
+  } else if (player1Score > player2Score) {
     return '\tYOU WON';
-  }
-  if (player1Score === player2Score) {
+  } else if (player1Score === player2Score) {
     return '\tITS A TIE...';
+  } else {
+    return '\tDEALER WON, YOU LOSE';
   }
-  return '\tDEALER WON, YOU LOSE';
 }
+/**
+ * Prints out the game with both hands and winning message
+ * @param {obj{hand[],handValue}} player1
+ * @param {obj{hand[],handValue}} player2
+ * @param {string} winningMessage
+ */
 function printGame(player1, player2, winningMessage) {
   console.clear();
   console.log(
--- test.js
+++
@@ -1,8 +0,0 @@
-const arr = [1, 2, 3, 4, 5, 6, 7];
-
-const pureSplice = (array, start, amount) => [
-  ...array.slice(0, start),
-  ...array.slice(start + amount),
-  // if you dont give slice a second arg it goes to end of array
-];
-console.log(pureSplice(arr, 0, 2));
